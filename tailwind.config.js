8c44bbec80bc7fb3b4dfe52937491dd2d3c2eaa2
Vikas <vk.pratap98@gmail.com>
2022-05-22 23:13:27 +0530

    setup done and mobile view partially done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily:{
+      'myLandingPageFontOne': 'myLandingPageFontOne',
+      'myLandingPageFontTwo': 'myLandingPageFontTwo',
+      'myLandingPageFontThree': 'myLandingPageFontThree',
+      'myLandingPageFontFour': 'myLandingPageFontFour',
+      'myLandingPageFontFive': 'myLandingPageFontFive',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./images/bg.jpg')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
+  },
+  plugins: [],
+}
